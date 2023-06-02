cfb20479c89d0568f508dc7b28fb51554e573fb6
juliomarinho <julio.m4r1nh0@gmail.com>
2023-06-02 00:19:55 -0300

    superchats_v1
    superchats_v1

---
+++ frontend/src/App.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import Routes from "./routes";
+import "react-toastify/dist/ReactToastify.css";
+
+import { createTheme, ThemeProvider } from "@material-ui/core/styles";
+import { ptBR } from "@material-ui/core/locale";
+
+const App = () => {
+	const [locale, setLocale] = useState();
+
+  const theme = createTheme(
+    {
+      scrollbarStyles: {
+        '&::-webkit-scrollbar': {
+          width: '8px',
+          height: '8px',
+        },
+        '&::-webkit-scrollbar-thumb': {
+          boxShadow: 'inset 0 0 6px rgba(0, 0, 0, 0.3)',
+          backgroundColor: '#e8e8e8',
+        },
+      },
+      palette: {
+        primary: { main: "#5578eb" },
+        default: "#f5f5ff",
+        background: {
+          default: "#fff",
+        },
+      },
+	danger: { main: '#FF5757' },
+    },
+    locale
+  );
+
+	useEffect(() => {
+		const i18nlocale = localStorage.getItem("i18nextLng");
+		const browserLocale =
+			i18nlocale.substring(0, 2) + i18nlocale.substring(3, 5);
+
+		if (browserLocale === "ptBR") {
+			setLocale(ptBR);
+		}
+	}, []);
+
+	return (
+		<ThemeProvider theme={theme}>
+			<Routes />
+		</ThemeProvider>
+	);
+};
+
+export default App;
